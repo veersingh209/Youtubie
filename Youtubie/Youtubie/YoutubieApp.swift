c4b288b98de4c5bade0d8a45c27acf36f546b105
Veer Singh <veersingh209@icloud.com>
2022-08-02 15:13:17 -0700

    Inital project commit

---
+++ Youtubie/Youtubie/YoutubieApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubieApp.swift
+//  Youtubie
+//
+//  Created by Veer Singh on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct YoutubieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
