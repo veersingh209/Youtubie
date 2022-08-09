9ffed90aafa925a061eb63f130c0ee8b2d025e31
Veer Singh <veersingh209@icloud.com>
2022-08-09 08:55:50 -0700

    Youtube API Contants

---
+++ Youtubie/Youtubie/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Youtubie
+//
+//  Created by Veer Singh on 8/9/22.
+//
+
+import Foundation
+
+class Constants {
+    
+    static let API_KEY = "AIzaSyAgRrxwbu1IVeDDVGDo_m1hJ7sqRyT6w5U"
+    static let API_URL = "https://www.googleapis.com/youtube/v3"
+}
